2343dbf28e292d2d2cb4702553623cff00d1f262
akashengine <vellore.akash@groupdrishti.in>
2024-10-06 16:12:00 +0530

    Create subject_data.py

---
+++ subject_data.py
@@ -0,0 +1,47 @@
+SUBJECTS = ["Economy", "Geography", "History", "Miscellaneous- Factual, Static GK", "Polity", "Science+ Sci.Tech.+ Computer"]
+
+TOPICS = {
+    "Economy": ["General"],
+    "Geography": ["Environment and ecology", "Indian Geography", "Physical Geography", "World Geography"],
+    "History": ["Ancient History", "Art and Culture", "Medieval History", "Modern History", "World History"],
+    "Miscellaneous- Factual, Static GK": ["General"],
+    "Polity": ["General"],
+    "Science+ Sci.Tech.+ Computer": ["Biology", "Chemistry", "Computer & IT", "Science and Technology", "Physics"]
+}
+
+SUB_TOPICS = {
+    "Environment and ecology": ["General"],
+    "Indian Geography": ["General"],
+    "Physical Geography": ["General"],
+    "World Geography": ["General"],
+    "Ancient History": ["General"],
+    "Art and Culture": ["General"],
+    "Medieval History": ["General"],
+    "Modern History": ["General"],
+    "World History": ["General"],
+    "Biology": ["Botany", "Zoology"],
+    "Chemistry": ["General"],
+    "Computer & IT": ["General"],
+    "Science and Technology": ["General"],
+    "Physics": ["General"]
+}
+
+PDF_NAMES = {
+    "Economy": ["BB-Economics.pdf", "KP-Economics.pdf", "PB-Economy.pdf"],
+    "Environment and ecology": ["BB-Ecology.pdf"],
+    "Indian Geography": ["BB-Geography Of India.pdf", "KP-Gepgraphy Of India.pdf"],
+    "Physical Geography": ["KP-Physical Geography.pdf"],
+    "World Geography": ["BB-World Geography.pdf", "KP-World Geography.pdf"],
+    "Ancient History": ["BB Ancient Indian History.pdf", "KP-Indian History.pdf", "PB-Ancient History.pdf"],
+    "Art and Culture": ["BB-Art & Culture.pdf", "KP-Indian Art Culture.pdf"],
+    "Medieval History": ["BB-Medieval Indian History.pdf", "PB-Medieval History.pdf"],
+    "Modern History": ["4. PB-Modern History.pdf", "BB-Modern Indian History.pdf"],
+    "World History": ["BB-World History.pdf", "KP-World History.pdf"],
+    "Miscellaneous- Factual, Static GK": ["1. Static GK PB.pdf", "BB-Census.pdf", "BB-Practice Sets.pdf", "KP-Books & Authors.pdf", "KP-Discoveries & Inventione.pdf", "KP-Honours & Awards.pdf", "KP-Important Decades Years & Days.pdf", "KP-International Evevts.pdf", "KP-Miscellaneous.pdf", "KP-National Events.pdf", "KP-Sports.pdf", "KP-Uno, Other Interanational &.pdf"],
+    "Polity": ["5. PB-Polity.pdf", "BB-Indian Polity.pdf", "KP-Indian Polity Constitution.pdf"],
+    "Biology": ["BB-Botany.pdf", "BB-Zoology.pdf", "KP-Bilogy (Zoology).pdf", "PB- Biology.pdf"],
+    "Chemistry": ["BB-Chemistry.pdf", "KP-Chemistry.pdf", "PB-Chemistry.pdf"],
+    "Computer & IT": ["KP-Compurer & IT.pdf"],
+    "Science and Technology": ["KP-Science and Technology.pdf"],
+    "Physics": ["BB-Physics.pdf", "KP-Physics.pdf", "PB- Physics.pdf"]
+}
