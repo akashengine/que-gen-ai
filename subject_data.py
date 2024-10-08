3357c3a23fa9eeed8da0c217ffcc08445c712920
akashengine <vellore.akash@groupdrishti.in>
2024-10-08 13:26:00 +0530

    Update subject_data.py

--- subject_data.py
+++ subject_data.py
@@ -1,37 +1,134 @@
-SUBJECTS = ["Economy", "Geography", "History", "Miscellaneous- Factual, Static GK", "Polity", "Science+ Sci.Tech.+ Computer"]
+SUBJECTS = ["English for SSC - All exams", "General Knowledge", "Mathematics", "Reasoning"]
 
 TOPICS = {
-    "Economy": ["General"],
-    "Geography": ["Environment and ecology", "Indian Geography", "Physical Geography", "World Geography"],
-    "History": ["Ancient History", "Art and Culture", "Medieval History", "Modern History", "World History"],
-    "Miscellaneous- Factual, Static GK": ["General"],
-    "Polity": ["General"],
-    "Science+ Sci.Tech.+ Computer": ["Biology", "Chemistry", "Computer & IT", "Science and Technology", "Physics"]
+    "English for SSC - All exams": [
+        "Common Error", "Misspelt Words", "Arrangement of Sentences", "Cloze test", "Comprehension", "Homonyms", 
+        "Active Passive", "Sentence Improvement", "Transformation of sentences", "Narration", "Fill in the Blanks", 
+        "Synonyms", "Antonyms", "One Word Substitution", "Idioms and Phrases"
+    ],
+    "General Knowledge": [
+        "Economy", "Geography", "History", "Miscellaneous- Factual, Static GK", "Polity", "Science+ Sci.Tech.+ Computer"
+    ],
+    "Mathematics": [
+        "Algebra", "Average", "Boat and Stream", "Compound Interest", "Coordinate Geometry", "Data Interpretation", 
+        "Discount", "Geometry", "HCF and LCM", "Height and Distance", "Linear Circular Race", "Mean, Median and Mode", 
+        "Mensuration", "Mixture and Alligation", "Number System", "Partnership", "Percentage", "Pipe and Cistern", 
+        "Probability", "Profit and Loss", "Ratio and Proportion", "Simple Interest", "Simplification", 
+        "Train, Speed and Distance", "Trigonometry", "Work and Time"
+    ],
+    "Reasoning": [
+        "Missing Number", "Age", "Analogy", "Analogy Non-Verbal", "Arithmetic Reasoning", "Blood Relations", 
+        "Coding-Decoding", "Completion of Figures", "Counting of Figures", "Cube and Dice", "Data Sufficiency", 
+        "Direction", "Embedded Figures", "Inequality", "Mathematical Operations", "Mirror and Water Images", 
+        "Odd one out", "Odd one out Non-Verbal", "Order and Ranking", "Paper cut and fold", "Puzzles", 
+        "Series", "Series Non-Verbal", "Sitting Arrangement", "Statement and Conclusion", "Syllogism", 
+        "Venn Diagram", "Word Arrangement", "Word Formation"
+    ]
 }
 
 PDF_NAMES = {
-    "Economy": ["BB-Economics.pdf", "KP-Economics.pdf", "PB-Economy.pdf"],
-    "Geography": {
-        "Environment and ecology": ["BB-Ecology.pdf"],
-        "Indian Geography": ["BB-Geography Of India.pdf", "KP-Gepgraphy Of India.pdf"],
-        "Physical Geography": ["KP-Physical Geography.pdf"],
-        "World Geography": ["BB-World Geography.pdf", "KP-World Geography.pdf"],
-        "General": ["PB-Geography.pdf"]
+    "English for SSC - All exams": {
+        "Common Error": ["KP-Common Error.pdf", "PB-Spot The Error.pdf"],
+        "Misspelt Words": ["KP-Selection Of Mis Spelt Word.pdf", "PB-Spelling Check.pdf"],
+        "Arrangement of Sentences": ["KP-Arrangement Of Sentences.pdf"],
+        "Cloze test": ["KP-Cloze Test.pdf", "PB-Cloze Test.pdf"],
+        "Comprehension": ["KP-Comprehension Test.pdf"],
+        "Homonyms": ["PB-Homonyms.pdf"],
+        "Active Passive": ["PB-Active Passive.pdf"],
+        "Sentence Improvement": ["KP-Improvement Of Sentence.pdf", "PB-Sentence Improvement.pdf"],
+        "Transformation of sentences": ["KP-Transformation Of Sentences.pdf"],
+        "Narration": ["KP-Direct Indirect Speech.pdf", "PB-Narration.pdf"],
+        "Fill in the Blanks": ["KP-Fill In The Blanks.pdf", "PB-Fill In The Blanks.pdf"],
+        "Synonyms": ["KP-Synonyms.pdf"],
+        "Antonyms": ["KP-Antonyms.pdf", "PB-Antonyms.pdf"],
+        "One Word Substitution": ["KP-One Word Substitution.pdf"],
+        "Idioms and Phrases": ["KP-Idioms Phrases.pdf", "PB-Idioms.pdf"]
     },
-    "History": {
-        "Ancient History": ["BB Ancient Indian History.pdf", "KP-Indian History.pdf", "PB-Ancient History.pdf"],
-        "Art and Culture": ["BB-Art & Culture.pdf", "KP-Indian Art Culture.pdf"],
-        "Medieval History": ["BB-Medieval Indian History.pdf", "PB-Medieval History.pdf"],
-        "Modern History": ["4. PB-Modern History.pdf", "BB-Modern Indian History.pdf"],
-        "World History": ["BB-World History.pdf", "KP-World History.pdf"]
+    "General Knowledge": {
+        "Economy": ["BB-Economics.pdf", "KP-Economics.pdf", "PB-Economy.pdf"],
+        "Geography": {
+            "Environment and ecology": ["BB-Ecology.pdf"],
+            "Indian Geography": ["BB-Geography Of India.pdf", "KP-Geography Of India.pdf"],
+            "Physical Geography": ["KP-Physical Geography.pdf"],
+            "World Geography": ["BB-World Geography.pdf", "KP-World Geography.pdf"],
+            "General": ["PB-Geography.pdf"]
+        },
+        "History": {
+            "Ancient History": ["BB-Ancient Indian History.pdf", "KP-Indian History.pdf", "PB-Ancient History.pdf"],
+            "Art and Culture": ["BB-Art & Culture.pdf", "KP-Indian Art Culture.pdf"],
+            "Medieval History": ["BB-Medieval Indian History.pdf", "PB-Medieval History.pdf"],
+            "Modern History": ["PB-Modern History.pdf", "BB-Modern Indian History.pdf"],
+            "World History": ["BB-World History.pdf", "KP-World History.pdf"]
+        },
+        "Miscellaneous- Factual, Static GK": [
+            "Static GK PB.pdf", "BB-Census.pdf", "BB-Practice Sets.pdf", "KP-Books & Authors.pdf", 
+            "KP-Discoveries & Inventions.pdf", "KP-Honours & Awards.pdf", "KP-Important Decades Years & Days.pdf", 
+            "KP-International Events.pdf", "KP-Miscellaneous.pdf", "KP-National Events.pdf", "KP-Sports.pdf", 
+            "KP-Uno, Other International.pdf"
+        ],
+        "Polity": ["PB-Polity.pdf", "BB-Indian Polity.pdf", "KP-Indian Polity Constitution.pdf"],
+        "Science+ Sci.Tech.+ Computer": {
+            "Biology": ["BB-Botany.pdf", "BB-Zoology.pdf", "KP-Biology (Zoology).pdf", "PB-Biology.pdf"],
+            "Chemistry": ["BB-Chemistry.pdf", "KP-Chemistry.pdf", "PB-Chemistry.pdf"],
+            "Computer & IT": ["KP-Computer & IT.pdf"],
+            "Science and Technology": ["KP-Science and Technology.pdf"],
+            "Physics": ["BB-Physics.pdf", "KP-Physics.pdf", "PB-Physics.pdf"]
+        }
     },
-    "Miscellaneous- Factual, Static GK": ["1. Static GK PB.pdf", "BB-Census.pdf", "BB-Practice Sets.pdf", "KP-Books & Authors.pdf", "KP-Discoveries & Inventione.pdf", "KP-Honours & Awards.pdf", "KP-Important Decades Years & Days.pdf", "KP-International Evevts.pdf", "KP-Miscellaneous.pdf", "KP-National Events.pdf", "KP-Sports.pdf", "KP-Uno, Other Interanational &.pdf"],
-    "Polity": ["5. PB-Polity.pdf", "BB-Indian Polity.pdf", "KP-Indian Polity Constitution.pdf"],
-    "Science+ Sci.Tech.+ Computer": {
-        "Biology": ["BB-Botany.pdf", "BB-Zoology.pdf", "KP-Bilogy (Zoology).pdf", "PB- Biology.pdf"],
-        "Chemistry": ["BB-Chemistry.pdf", "KP-Chemistry.pdf", "PB-Chemistry.pdf"],
-        "Computer & IT": ["KP-Compurer & IT.pdf"],
-        "Science and Technology": ["KP-Science and Technology.pdf"],
-        "Physics": ["BB-Physics.pdf", "KP-Physics.pdf", "PB- Physics.pdf"]
+    "Mathematics": {
+        "Algebra": ["PB-Algebra.pdf"],
+        "Average": ["PB-Average.pdf"],
+        "Boat and Stream": ["PB-Boat and Stream.pdf"],
+        "Compound Interest": ["PB-Compound Interest.pdf"],
+        "Coordinate Geometry": ["PB-Coordinate Geometry.pdf"],
+        "Data Interpretation": ["PB-Data Interpretation.pdf"],
+        "Discount": ["PB-Discount.pdf"],
+        "Geometry": ["PB-Geometry.pdf"],
+        "HCF and LCM": ["PB-HCF and LCM.pdf"],
+        "Height and Distance": ["PB-Height and Distance.pdf"],
+        "Linear Circular Race": ["PB-Linear Circular Race.pdf"],
+        "Mean, Median and Mode": ["PB-Mean, Median & Mode.pdf"],
+        "Mensuration": ["PB-Mensuration.pdf"],
+        "Mixture and Alligation": ["PB-Mixture and Alligation.pdf"],
+        "Number System": ["PB-Number System.pdf"],
+        "Partnership": ["PB-Partnership.pdf", "PB-Percentage.pdf"],
+        "Percentage": ["PB-Percentage.pdf"],
+        "Pipe and Cistern": ["PB-Pipe and Cistern.pdf"],
+        "Probability": ["PB-Probability.pdf"],
+        "Profit and Loss": ["PB-Profit and Loss.pdf"],
+        "Ratio and Proportion": ["PB-Ratio & Proportion.pdf"],
+        "Simple Interest": ["PB-Simple Interest.pdf"],
+        "Simplification": ["PB-Simplification.pdf"],
+        "Train, Speed and Distance": ["PB-Time, Speed and Distance.pdf"],
+        "Trigonometry": ["PB-Trigonometry.pdf"],
+        "Work and Time": ["PB-Work and Time.pdf"]
+    },
+    "Reasoning": {
+        "Missing Number": ["PB-Missing Number.pdf"],
+        "Age": ["PB-Age.pdf"],
+        "Analogy": ["PB-Analogy.pdf", "PB-Analogy Non Verbal.pdf"],
+        "Arithmetic Reasoning": ["PB-Arithmetic Reasoning.pdf"],
+        "Blood Relations": ["PB-Blood Relations.pdf"],
+        "Coding-Decoding": ["PB-Coding Decoding.pdf"],
+        "Completion of Figures": ["PB-Completion Of Figure.pdf"],
+        "Counting of Figures": ["PB-Counting Of Figure.pdf"],
+        "Cube and Dice": ["PB-Cube and Dice.pdf"],
+        "Data Sufficiency": ["PB-Data Sufficiency.pdf"],
+        "Direction": ["PB-Direction.pdf"],
+        "Embedded Figures": ["PB-Embedded Figure.pdf"],
+        "Inequality": ["PB-Inequality.pdf"],
+        "Mathematical Operations": ["PB-Mathematical.pdf"],
+        "Mirror and Water Images": ["PB-Mirror Water Image.pdf"],
+        "Odd one out": ["PB-Odd one Out.pdf", "PB-Odd One Out Non Verbal.pdf"],
+        "Order and Ranking": ["PB-Order & Ranking.pdf"],
+        "Paper cut and fold": ["PB-Paper Cut And Fold.pdf"],
+        "Puzzles": ["PB-Puzzle.pdf"],
+        "Series": ["PB-Series.pdf", "PB-Series Non-Verbal.pdf"],
+        "Sitting Arrangement": ["PB-Sitting Arrangement.pdf"],
+        "Statement and Conclusion": ["PB-Statement & Conclusion.pdf"],
+        "Syllogism": ["PB-Syllogism.pdf"],
+        "Venn Diagram": ["PB-Venn Diagram.pdf"],
+        "Word Arrangement": ["PB-Word Arrangement.pdf"],
+        "Word Formation": ["PB-Word Formation.pdf"]
     }
 }
